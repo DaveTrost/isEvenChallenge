0bbb1bf7c56882aedb35abbdd209506fa0ee78f5
Dave Trost <davetrost@gmail.com>
2019-07-11 21:40:33 -0700

    adding the is-even file

---
+++ is-even.js
@@ -0,0 +1,11 @@
+function isEven(number) {
+    // const value = number % 2;
+    // if(value === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return (number % 2 === 0);
+}
+export default isEven;
