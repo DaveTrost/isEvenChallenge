eee51c914f9ab00dd98303ed0b78ecc62c8496c0
Dave Trost <davetrost@gmail.com>
2019-07-11 22:01:44 -0700

    finishing touches: enable enter key, update links

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ import isEven from './is-even.js';
 const inputX = document.getElementById('x');
 const button = document.getElementById('button');
 button.onclick = buttonClicked;
+inputX.addEventListener('keyup', function(event) {
+    // Execute a function when the user releases a key on the keyboard
+    // Number 13 is the "Enter" key on the keyboard
+    if(event.keyCode === 13) {
+        document.getElementById('button').click();
+    }
+});
 const evenImage = document.getElementById('even');
 const oddImage = document.getElementById('odd');
 
 function buttonClicked() {
     console.log('button clicked');
-    if(inputX.value === "") {
+    if(inputX.value === '') {
         evenImage.classList.add('hidden');
         oddImage.classList.add('hidden');
     } else if(isEven(inputX.value)) {
